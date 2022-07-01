0904c787969c8a444436eb2320a1a8e62e44fc1a
Yu <haoyuhaoyu@google.com>
2022-07-01 17:51:04 +0000

    update for android common kernel

--- GenKernelBitcode.go
+++ GenKernelBitcode.go
@@ -13,15 +13,17 @@ import (
 // Build one module or whole kernel, e.g., module, kernel
 var cmd = flag.String("cmd", "kernel", "Build one module or whole kernel, e.g., module, kernel")
 
-// "The path of kernel, e.g., linux"
-var path = "."
+// The path of kernel, e.g., linux
+var path = flag.String("path", ".", "the path of kernel")
 
 // "is -save-temps or not"
 // two kinds of two to generate bitcode
-var IsSaveTemps = flag.Bool("isSaveTemp", true, "use -save-temps or -emit-llvm")
+var IsSaveTemps = flag.Bool("isSaveTemp", false, "use -save-temps or -emit-llvm")
 
 var CC = flag.String("CC", "clang", "Name of CC")
 var LD = flag.String("LD", "llvm-link", "Name of LD")
+
+var LLD = flag.String("LLD", "ld.lld", "Name of LD")
 
 // ToolChain of clang and llvm-link
 // ToolChain   = "/home/yhao016/data/benchmark/hang/kernel/toolchain/clang-r353983c/bin/"
@@ -51,12 +53,13 @@ const (
 	// FlagCCNoNumber add label to basic blocks and variables
 	FlagCCNoNumber = " -fno-discard-value-names"
 
-	NameLD = "llvm-link"
-	FlagLD = " -v"
+	NameLD    = "llvm-link"
+	FlagLD    = " -v "
+	FlagOutLD = " -o "
 
 	CmdLinkVmlinux = "llvm-link -v -o built-in.bc arch/x86/kernel/head_64.bc arch/x86/kernel/head64.bc arch/x86/kernel/ebda.bc arch/x86/kernel/platform-quirks.bc init/built-in.bc usr/built-in.bc arch/x86/built-in.bc kernel/built-in.bc certs/built-in.bc mm/built-in.bc fs/built-in.bc ipc/built-in.bc security/built-in.bc crypto/built-in.bc block/built-in.bc lib/built-in.bc arch/x86/lib/built-in.bc lib/lib.bc arch/x86/lib/lib.bc drivers/built-in.bc sound/built-in.bc net/built-in.bc virt/built-in.bc arch/x86/pci/built-in.bc arch/x86/power/built-in.bc arch/x86/video/built-in.bc\n"
 	// CmdTools skip the cmd with CmdTools
-	CmdTools = "-BUILD_STR(s)=$(pound)s"
+	CmdTools = "BUILD_STR(s)=$(pound)s"
 )
 
 func getCmd(cmdFilePath string) string {
@@ -85,10 +88,10 @@ func getCmd(cmdFilePath string) string {
 		for _, eachLine := range text {
 			if strings.HasPrefix(eachLine, PrefixCmd) {
 				i := strings.Index(eachLine, ":=")
-				// fmt.Println("Index: ", i)
 				if i > -1 {
 					cmd := eachLine[i+3:]
 					res = cmd
+					break
 				} else {
 					fmt.Println("Cmd Index not found")
 					fmt.Println(eachLine)
@@ -100,10 +103,11 @@ func getCmd(cmdFilePath string) string {
 		}
 	}
 	res += "\n"
+	res = res[strings.Index(res, ""):]
 	return res
 }
 
-func replaceCC(cmd string) string {
+func handleCC(cmd string) string {
 	res := ""
 	if i := strings.Index(cmd, " -c "); i > -1 {
 
@@ -146,18 +150,77 @@ func replaceCC(cmd string) string {
 		fmt.Println("CC Index not found")
 		fmt.Println(cmd)
 	}
+
+	res = strings.Replace(res, *CC, filepath.Join(*ToolChain, NameClang), -1)
+	res = strings.Replace(res, " -Os ", " -O0 ", -1)
+	res = strings.Replace(res, " -O3 ", " -O0 ", -1)
+	res = strings.Replace(res, " -O2 ", " -O0 ", -1)
+	res = strings.Replace(res, " -fno-var-tracking-assignments ", "  ", -1)
+	res = strings.Replace(res, " -fconserve-stack ", "  ", -1)
+	res = strings.Replace(res, " -march=armv8-a+crypto ", "  ", -1)
+	res = strings.Replace(res, " -mno-fp-ret-in-387 ", "  ", -1)
+	res = strings.Replace(res, " -mskip-rax-setup ", "  ", -1)
+	res = strings.Replace(res, " -ftrivial-auto-var-init=zero ", "  ", -1)
+
 	return res
 }
 
-func replaceLD(cmd string) string {
+func handleSuffixCCWithLD(cmd string, path string) string {
+	res := ""
+	if strings.Index(cmd, "@") > -1 {
+		fileName := cmd[strings.Index(cmd, "@")+1 : len(cmd)-1]
+		filePath := filepath.Join(path, fileName)
+		file, err := os.Open(filePath)
+		if err != nil {
+			log.Println("handleSuffixCCWithLD file error: ")
+			log.Fatal(err)
+		}
+		defer func(file *os.File) {
+			err := file.Close()
+			if err != nil {
+
+			}
+		}(file)
+
+		scanner := bufio.NewScanner(file)
+		scanner.Split(bufio.ScanLines)
+
+		var text []string
+		for scanner.Scan() {
+			text = append(text, scanner.Text())
+		}
+
+		if err := scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
+
+		res += filepath.Join(*ToolChain, NameLD)
+		res += FlagLD
+		res += FlagOutLD
+		res += cmd[strings.Index(cmd, FlagOutLD)+len(FlagOutLD) : strings.Index(cmd, "@")]
+
+		for _, s := range text {
+			res += s + " "
+		}
+
+		res = strings.Replace(res, ".o ", ".bc ", -1)
+		res += "\n"
+
+	} else {
+		fmt.Println("handleSuffixCCWithLD cmd error: " + cmd)
+	}
+	return res
+}
+
+func handleLD(cmd string) string {
 
 	replace := func(cmd string, i int, length int) string {
 		res := ""
 		cmd = cmd[i+length:]
 		if strings.Count(cmd, ".") > 1 {
-			res += NameLD
+			res += filepath.Join(*ToolChain, NameLD)
 			res += FlagLD
-			res += " -o "
+			res += FlagOutLD
 			res += cmd
 			if strings.Contains(res, "drivers/of/unittest-data/built-in.o") {
 				res = ""
@@ -173,7 +236,7 @@ func replaceLD(cmd string) string {
 		res = strings.Replace(res, "rodata_objcopy.bc", "rodata.bc", -1)
 		res = strings.Replace(res, " drivers/of/unittest-data/built-in.bc", "", -1)
 
-		// for ";"
+		// for multiply cmd or ";" pick the first one
 		if strings.Count(res, ";") > 1 {
 			i := strings.Index(res, ";")
 			res = res[:i] + "\n"
@@ -201,34 +264,59 @@ func replaceLD(cmd string) string {
 	return res
 }
 
-func get_linked_target(cmd string) string {
+func handleLTO(cmd string) string {
 	res := ""
-	if strings.Contains(cmd, "llvm-link -v -o") {
-		res = cmd[len("llvm-link -v -o") : strings.Index(cmd, ".bc")+3]
-	}
+	res += filepath.Join(*ToolChain, NameLD)
+	res += FlagLD
+
+	cmd = cmd[strings.Index(cmd, FlagOutLD):]
+	cmd = strings.Replace(cmd, " --whole-archive ", "", -1)
+	cmd = strings.Replace(cmd, ".o", ".bc", -1)
+
+	res += cmd
 	return res
 }
 
-func build(moduleDirPath string) string {
+func handleKO(cmd string) (string, string) {
+	res := ""
+	res += filepath.Join(*ToolChain, NameLD)
+	res += FlagLD
+	res += FlagOutLD
+
+	// for multiply cmd or ";" pick the first one
+	if strings.Count(cmd, ";") > 1 {
+		i := strings.Index(cmd, ";")
+		cmd = cmd[:i] + "\n"
+	}
+
+	cmd = cmd[strings.Index(cmd, FlagOutLD)+len(FlagOutLD):]
+	cmd = strings.Replace(cmd, ".ko", ".ko.bc", -1)
+	cmd = strings.Replace(cmd, ".o", ".bc", -1)
+
+	moduleFile := cmd[:strings.Index(cmd, ".ko.bc")+len(".ko.bc")]
+	res += cmd
+
+	return res, moduleFile
+}
+
+func build(kernelPath string) string {
 	res1 := ""
-	err := filepath.Walk(moduleDirPath,
+
+	var SuffixCCWithLD []string
+
+	err := filepath.Walk(kernelPath,
 		func(path string, info os.FileInfo, err error) error {
 			if err != nil {
 				return err
 			}
-			if strings.HasSuffix(info.Name(), SuffixCC) {
+			if strings.HasSuffix(info.Name(), SuffixCC) && !strings.HasSuffix(info.Name(), SuffixLTO) {
 				cmd := getCmd(path)
 				if strings.HasPrefix(cmd, *CC) {
-					res2 := replaceCC(cmd)
-					res2 = strings.Replace(res2, *CC, NameClang, -1)
-					res2 = strings.Replace(res2, " -Os ", " -O0 ", -1)
-					res2 = strings.Replace(res2, " -O3 ", " -O0 ", -1)
-					res2 = strings.Replace(res2, " -O2 ", " -O0 ", -1)
-					res2 = strings.Replace(res2, " -fno-var-tracking-assignments ", "  ", -1)
-					res2 = strings.Replace(res2, " -fconserve-stack ", "  ", -1)
-					res2 = strings.Replace(res2, " -march=armv8-a+crypto ", "  ", -1)
+					res2 := handleCC(cmd)
 					//res2 = strings.Replace(res2, IncludeOld, IncludeNew, -1)
 					res1 += res2
+				} else if strings.Index(cmd, *LLD) > -1 {
+					SuffixCCWithLD = append(SuffixCCWithLD, cmd)
 				} else {
 					fmt.Println(*CC + " not found")
 					fmt.Println(path)
@@ -241,9 +329,14 @@ func build(moduleDirPath string) string {
 		log.Println(err)
 	}
 
+	res3 := ""
+	for _, cmd := range SuffixCCWithLD {
+		res3 = handleSuffixCCWithLD(cmd, kernelPath) + res3
+	}
+
 	res2 := ""
-	module_file := ""
-	err = filepath.Walk(moduleDirPath,
+	moduleFiles := ""
+	err = filepath.Walk(kernelPath,
 		func(path string, info os.FileInfo, err error) error {
 			if err != nil {
 				return err
@@ -251,45 +344,39 @@ func build(moduleDirPath string) string {
 			if strings.HasSuffix(info.Name(), SuffixLD) {
 				//for built-in module built-in.a
 				cmd := getCmd(path)
-				res2 = replaceLD(cmd) + res2
-
-				//} else if strings.HasSuffix(info.Name(), SuffixCC) {
-				//	cmd := getCmd(path)
-				//	res2 = replaceLD(cmd) + res2
+				res2 = handleLD(cmd) + res2
 
 			} else if strings.HasSuffix(info.Name(), SuffixLTO) {
 				//for external module *.lto
 				cmd := getCmd(path)
-				cmd = cmd[strings.Index(cmd, "--whole-archive")+len("--whole-archive") : len(cmd)-1]
-				cmd = strings.Replace(cmd, ".o", ".bc", -1)
-				module_file = cmd + module_file
+				res2 = handleLTO(cmd) + res2
 
 			} else if strings.HasSuffix(info.Name(), SuffixKO) {
 				//for external module *.ko
-				cmd := getCmd(path)
-				cmd = cmd[strings.Index(cmd, "--whole-archive")+len("--whole-archive") : len(cmd)-1]
-				cmd = strings.Replace(cmd, ".o", ".bc", -1)
-				module_file = cmd + module_file
-
+				cmd, moduleFile := handleKO(getCmd(path))
+				res2 = cmd + res2
+				moduleFiles = moduleFile + " " + moduleFiles
 			}
 
 			return nil
 		})
+
 	if err != nil {
 		log.Println(err)
 	}
-	fmt.Println("module_file")
-	fmt.Println(module_file)
 
-	return res1 + res2
+	fmt.Println("moduleFiles: ")
+	fmt.Println(moduleFiles)
+
+	return res1 + res3 + res2 + "\n# " + moduleFiles + "\n"
 }
 
 func generateScript(path string, cmd string) {
 	res := "#!/bin/bash\n"
 	res += cmd
 
-	pathScript := filepath.Join(path, NameScript)
-	_ = os.Remove(pathScript)
+	pathScript := filepath.Join(NameScript)
+	_ = os.RemoveAll(pathScript)
 	fmt.Printf("script path : bash %s\n", pathScript)
 	f, err := os.OpenFile(pathScript, os.O_WRONLY|os.O_CREATE, 0666)
 	if err != nil {
@@ -303,24 +390,27 @@ func generateScript(path string, cmd string) {
 	}(f)
 
 	_, _ = f.WriteString(res)
+	_, _ = f.WriteString("\n# path: " + path + "\n")
 }
 
 func main() {
 	flag.Parse()
 
+	*path, _ = filepath.Abs(*path)
+
 	switch *cmd {
 	case "module":
 		{
-			fmt.Printf("Build one module\n")
-			res := build(path)
-			generateScript(path, res)
+			fmt.Printf("Build module\n")
+			res := build(*path)
+			generateScript(*path, res)
 		}
 	case "kernel":
 		{
-			fmt.Printf("Build whole kernel\n")
-			res := build(path)
+			fmt.Printf("Build kernel and external module\n")
+			res := build(*path)
 			res += CmdLinkVmlinux
-			generateScript(path, res)
+			generateScript(*path, res)
 		}
 	default:
 		fmt.Printf("cmd is invalid\n")
