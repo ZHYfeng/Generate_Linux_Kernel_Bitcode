dceb44fc39e773e1bc01d0b45879f95c3aec594e
Yu Hao <yhao016@ucr.edu>
2023-04-02 15:22:15 -0700

    update

--- v6.1/KernelBitcode.go
+++ v6.1/KernelBitcode.go
@@ -31,7 +31,7 @@ var STRIP = flag.String("STRIP", "llvm-strip", "Name of STRIP")
 // ToolChain of clang and llvm-link
 var ToolChain = flag.String("toolchain", "", "Path of clang and llvm-link")
 
-var FlagCC = FlagAll + FlagCCNoNumber + FlagCCNoNumber
+var FlagCC = FlagAll + FlagCCNoNumber + FlagCCNoOptzns
 
 const (
 	PrefixCmd = "cmd_"
