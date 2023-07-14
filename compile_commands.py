9734f65b6efad358700ff419143d65cf88e6cab5
Yu Hao <yhao016@ucr.edu>
2023-07-14 14:10:27 -0700

    add a progress bar

--- compile_commands.py
+++ compile_commands.py
@@ -1,6 +1,7 @@
 import json
 import subprocess
 from concurrent.futures import ThreadPoolExecutor
+from tqdm import tqdm
 
 
 def preprocess_command(command):
@@ -16,12 +17,14 @@ def preprocess_command(command):
     modified_command = modified_command.replace(' -Os ', ' -O0 ')
     modified_command = modified_command.replace(' -O3 ', ' -O0 ')
     modified_command = modified_command.replace(' -O2 ', ' -O0 ')
-    modified_command = modified_command.replace(' -fno-var-tracking-assignments ', ' ')
+    modified_command = modified_command.replace(
+        ' -fno-var-tracking-assignments ', ' ')
     modified_command = modified_command.replace(' -fconserve-stack ', ' ')
     modified_command = modified_command.replace(' -march=armv8-a+crypto ', ' ')
     modified_command = modified_command.replace(' -mno-fp-ret-in-387 ', ' ')
     modified_command = modified_command.replace(' -mskip-rax-setup ', ' ')
-    modified_command = modified_command.replace(' -ftrivial-auto-var-init=zero ', ' ')
+    modified_command = modified_command.replace(
+        ' -ftrivial-auto-var-init=zero ', ' ')
     return modified_command
 
 
@@ -35,7 +38,8 @@ def execute_command(command):
 
 def execute_commands_concurrently(commands, max_workers):
     with ThreadPoolExecutor(max_workers=max_workers) as executor:
-        executor.map(execute_command, commands)
+        results = list(tqdm(executor.map(execute_command, commands),
+                       total=len(commands), desc='Executing Commands'))
 
 
 # Read compile_commands.json file
@@ -48,5 +52,5 @@ commands = [entry['command'] for entry in compile_commands]
 # Adjust the number of concurrent threads
 max_workers = 32  # Example: Set the maximum number of concurrent threads to 4
 
-# Execute the commands concurrently
+# Execute the commands concurrently with a progress bar
 execute_commands_concurrently(commands, max_workers)
